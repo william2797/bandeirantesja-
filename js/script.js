1b89d1abd3845ff1e5835f3d2bb8c419a0d0c3fe
William Souza <williamd400@gmail.com>
2024-07-05 21:41:40 -0300

    rep

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+
+const tabs = document.querySelectorAll('.tab-btn');
+
+tabs.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)));
+
+const tabClicked = (tab) => {
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tab.classList.add('active');
+
+    const contents = document.querySelectorAll('.content');
+    contents.forEach(content => content.classList.remove('show'));
+
+    const contentId = tab.getAttribute('content-id');
+    const content = document.getElementById(contentId);
+
+    content.classList.add('show');
+}
+
+const currentActiveTab = document.querySelector('.tab-btn.active');
+tabClicked(currentActiveTab);
+
+
+
+
+
+  
+//var copy = document.querySelector(".logos-slide").cloneNode(true);
+//document.querySelector(".logos").appendChild(copy);
---
+++ js/timeline.js
@@ -0,0 +1,26 @@
+export default timeline
+
+const elH = document.querySelectorAll(".timeline li > div");
+
+
+window.addEventListener("load", init);
+
+function init() {
+  setEqualHeights(elH);
+}
+
+
+function setEqualHeights(el) {
+  let counter = 0;
+  for (let i = 0; i < el.length; i++) {
+    const singleHeight = el[i].offsetHeight;
+
+    if (counter < singleHeight) {
+      counter = singleHeight;
+    }
+  }
+
+  for (let i = 0; i < el.length; i++) {
+    el[i].style.height = `${counter}px`;
+  }
+}
